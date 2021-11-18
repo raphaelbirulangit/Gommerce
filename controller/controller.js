07699426c9e349189425f17e0a4dd845de091ebd
raphaelbirulangit <raphaelbirulangit@gmail.com>
2021-11-18 13:29:18 +0700

    update express, migrasi, controller

---
+++ controller/controller.js
@@ -0,0 +1,20 @@
+const {Store, User} = require('../models')
+const {Op} = require('sequelize')
+
+class Controller {
+    static home(){
+
+    }
+
+    static login(){
+
+    }
+
+    static readStorebyId(){
+
+    }
+
+    static userDetail(){
+        
+    }
+}
